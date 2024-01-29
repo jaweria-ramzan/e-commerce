7434a70888162592974b2cf5178d080e6e0bb9b0
Jaweria Ramzan <riaramzanria@gmail.com>
2024-01-29 19:08:01 +0500

    e-commerce

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { ShoppingBag } from "lucide-react";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      
+    <div className="px-[124px] w-full h-svh mt-16">
+      <div className="font-bold text-3xl">Shopping Cart</div>
+      <div className="flex flex-col justify-center items-center mt-9">
+        <ShoppingBag size={150} />
+        <div className="font-bold text-3xl mt-3">
+          Your shopping bag is empty
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/views/Portion3.tsx
+++ src/views/Portion3.tsx
@@ -1,100 +1,100 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import Carousel from './Carousel'
+"use client";
+import React from "react";
+import Image from "next/image";
+import Carousel from "./Carousel";
 
-const Slides=[
+const Slides = [
   {
-    img:'/sl1.png',
-    id:1,
-    name:"Brushed Raglan Sweatshirt",
-    property:"Sweater",
-    price:"$195"
+    img: "/sl1.png",
+    id: 1,
+    name: "Brushed Raglan Sweatshirt",
+    property: "Sweater",
+    price: "$195",
   },
   {
-    img:'/sl2.png',
-    id:2,
-    name:"Cameryn Sash Tie Dress",
-    property:"Dress",
-    price:"$545"
+    img: "/sl2.png",
+    id: 2,
+    name: "Cameryn Sash Tie Dress",
+    property: "Dress",
+    price: "$545",
   },
   {
-    img:'/sl3.png',
-    id:3,
-    name:"Flex Sweatshirt",
-    property:"Sweater",
-    price:"$175"
+    img: "/sl3.png",
+    id: 3,
+    name: "Flex Sweatshirt",
+    property: "Sweater",
+    price: "$175",
   },
   {
-    img:'/sl4.png',
-    id:4,
-    name:"Flex Sweatpants",
-    property:"Pants",
-    price:"$175"
+    img: "/sl4.png",
+    id: 4,
+    name: "Flex Sweatpants",
+    property: "Pants",
+    price: "$175",
   },
   {
-    img:'/sl5.png',
-    id:5,
-    name:"Pink Fleece Sweatpants",
-    property:"Pants",
-    price:"$195"
+    img: "/sl5.png",
+    id: 5,
+    name: "Pink Fleece Sweatpants",
+    property: "Pants",
+    price: "$195",
   },
   {
-    img:'/sl6.png',
-    id:6,
-    name:"Lite Sweatpants",
-    property:"Pants",
-    price:"$150"
+    img: "/sl6.png",
+    id: 6,
+    name: "Lite Sweatpants",
+    property: "Pants",
+    price: "$150",
   },
   {
-    img:'/sl7.png',
-    id:7,
-    name:"Imperial Alpaca Hoodie",
-    property:"Jackets",
-    price:"$525"
+    img: "/sl7.png",
+    id: 7,
+    name: "Imperial Alpaca Hoodie",
+    property: "Jackets",
+    price: "$525",
   },
   {
-    img:'/sl8.png',
-    id:8,
-    name:"Flex Push Button Bomber",
-    property:"Jackets",
-    price:"$225"
+    img: "/sl8.png",
+    id: 8,
+    name: "Flex Push Button Bomber",
+    property: "Jackets",
+    price: "$225",
   },
   {
-    img:'/sl9.png',
-    id:9,
-    name:"Muscle Tank",
-    property:"T Shirts",
-    price:"$75"
+    img: "/sl9.png",
+    id: 9,
+    name: "Muscle Tank",
+    property: "T Shirts",
+    price: "$75",
   },
   {
-    img:'/sl10.png',
-    id:10,
-    name:"Brushed Bomber",
-    property:"Jackets",
-    price:"$225"
+    img: "/sl10.png",
+    id: 10,
+    name: "Brushed Bomber",
+    property: "Jackets",
+    price: "$225",
   },
   {
-    img:'/sl11.png',
-    id:11,
-    name:"Raglan Sweatshirt",
-    property:"Sweater",
-    price:"$195"
-  }
-]
+    img: "/sl11.png",
+    id: 11,
+    name: "Raglan Sweatshirt",
+    property: "Sweater",
+    price: "$195",
+  },
+];
 
 const Portion3 = () => {
   return (
-      <div className=' w-full h-[650px] mt-28'>
-        <div className='text-center'>
-         <span className='text-blue-600'> PRODUCTS</span> <br />
-         <span className='font-bold text-3xl'> Check What We Have</span>     
-        </div>
-        <div className='h-[580px] relative overflow-auto mt-9 p-5'>
-          <Carousel slides={Slides}></Carousel>
-        </div>
+    <div className=" w-full h-[650px] mt-28">
+      <div className="text-center">
+        <span className="text-blue-600"> PRODUCTS</span> <br />
+        <span className="font-bold text-3xl"> Check What We Have</span>
       </div>
-  )
-}
+      <div className="h-[580px] relative overflow-auto  mt-9 p-5">
+        <Carousel slides={Slides}></Carousel>
+      </div>
+    </div>
+  );
+};
 
-export default Portion3
+export default Portion3;
